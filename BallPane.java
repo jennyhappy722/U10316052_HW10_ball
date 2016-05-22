5cd0eda3f7a10979b69b865c50fd0f57a8fd666d
jennyhappy722 <u10316052@go.utaipei.edu.tw>
2016-05-22 15:57:38 +0800

    add button

--- BallPane.java
+++ BallPane.java
@@ -8,27 +8,32 @@ import javafx.util.Duration;
 import javafx.scene.control.*;
 
 public class BallPane extends Pane {
-  public final double radius = 20;
-  private double x = radius, y = radius;
-  private double dx = 1, dy = 1;
-  private  Circle circle;
-  private Timeline animation;
+  public  double radius = 20;
+  public double x = 20, y = 20;
+  public double dx = 1, dy = 1;
+
+  public  Circle circle;
+  Timeline animation;
 
 
   public BallPane() {
-	Button button=new Button("new ball");
+	Button button=new Button("Follow  Ball  Locus");
 			button.setOnAction(e ->{ 
-			animatee();				
-		});
+			addcircle();
+			//animatee();				
+	});
 	
 	circle = new Circle(x, y, radius);
 	button.relocate(50,0);
     circle.setFill(Color.GREEN);	// Set ball color	
     getChildren().addAll(button,circle); // Place a ball into this pane
 
+
     // Create an animation for moving the ball
     animation = new Timeline(
-      new KeyFrame(Duration.millis(50), e -> moveBall()));
+      new KeyFrame(Duration.millis(50), e ->{
+	   moveBall();
+	   }));
     animation.setCycleCount(Timeline.INDEFINITE);
     animation.play(); // Start animation
   }
@@ -38,7 +43,9 @@ public class BallPane extends Pane {
   public void animatee(){
 	
 	animation = new Timeline(
-    new KeyFrame(Duration.millis(50), e -> moveBall()));
+    new KeyFrame(Duration.millis(50), e ->{
+	moveBall();
+	}));
     animation.setCycleCount(Timeline.INDEFINITE);
     animation.play(); // Start animation	    
   } 
@@ -65,17 +72,19 @@ public class BallPane extends Pane {
     return animation.rateProperty();
   }
   
-  protected void moveBall() {
-    // Check boundaries
-    if (x < radius || x > getWidth() - radius) {
+  public void moveBall() {
+	// Check boundaries
+	
+    if (x < 20 || x > getWidth() - 20) {
       dx *= -1; // Change ball move direction
     }
-    if (y < radius || y > getHeight() - radius) {
+    if (y < 20 || y > getHeight() - 20) {
       dy *= -1; // Change ball move direction
     }
     // Adjust ball position
     x += dx;
     y += dy;
+	
     circle.setCenterX(x);
     circle.setCenterY(y);
   }
